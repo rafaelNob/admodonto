95c1cbd356518eda709588506cce8318f342b33d
rafael franca <rafael.franca@monitoratec.com.br>
2022-04-18 23:32:37 +0100

    adicionando menu horizontal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { pt_PT } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import {NzAvatarModule} from "ng-zorro-antd/avatar";
+
+registerLocaleData(pt);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        IconsProviderModule,
+        NzLayoutModule,
+        NzMenuModule,
+        NzCardModule,
+        NzGridModule,
+        NzStatisticModule,
+        NzAvatarModule
+    ],
+  providers: [{ provide: NZ_I18N, useValue: pt_PT }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
